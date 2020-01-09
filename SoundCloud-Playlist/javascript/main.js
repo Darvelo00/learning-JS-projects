8d9bc40a1712348d668cea1ae73f79b907917361
Daniel Arvelo <darvelo00@gmail.com>
2020-01-09 17:50:58 -0500

    Created SoundCloud Object

--- SoundCloud-Playlist/javascript/main.js
+++ SoundCloud-Playlist/javascript/main.js
@@ -2,19 +2,26 @@
 
 
 /* 2. Query Soundcloud API*/
-SC.initialize({
-    client_id: 'cd9be64eeb32d1741c17cb39e41d254d'
-  });
-  
-  // find all sounds of buskers licensed under 'creative commons share alike'
-  SC.get('/tracks', {
-    q: 'buskers'
-  }).then(function(tracks) {
-    console.log(tracks);
-  });
+
+var SoundCloudAPI = {};
+
+SoundCloudAPI.init = function() {
+    SC.initialize({
+        client_id: 'cd9be64eeb32d1741c17cb39e41d254d'
+    });
+}
+SoundCloudAPI.getTrack = function(inputValue) {
+    // find all sounds of q variable
+    SC.get('/tracks', {
+        q: inputValue
+    }).then(function(tracks) {
+        console.log(tracks);
+    });
+}
 
 /* 3. Display the cards*/
 
 
 /* 4. Add to playlist and play*/
-
+SoundCloudAPI.init();
+SoundCloudAPI.getTrack("Rilo Kiley");
