a37772d703dec14cc5b7e083eb6acf7206e7e853
Daniel Arvelo <darvelo00@gmail.com>
2020-01-09 17:21:44 -0500

    indented code and added client id

--- SoundCloud-Playlist/javascript/main.js
+++ SoundCloud-Playlist/javascript/main.js
@@ -3,12 +3,12 @@
 
 /* 2. Query Soundcloud API*/
 SC.initialize({
-    client_id: 'YOUR_CLIENT_ID'
+    client_id: 'cd9be64eeb32d1741c17cb39e41d254d'
   });
   
   // find all sounds of buskers licensed under 'creative commons share alike'
   SC.get('/tracks', {
-    q: 'buskers', license: 'cc-by-sa'
+    q: 'buskers'
   }).then(function(tracks) {
     console.log(tracks);
   });
