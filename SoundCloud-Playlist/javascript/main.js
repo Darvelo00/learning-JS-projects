e5988da8a3fb6dba28823921270602fb88322452
Daniel Arvelo <darvelo00@gmail.com>
2020-01-09 17:12:26 -0500

    connected SoundCloud API Search functionality

--- SoundCloud-Playlist/javascript/main.js
+++ SoundCloud-Playlist/javascript/main.js
@@ -1,8 +1,17 @@
 /* 1. Search Soundcload*/
 
 
-/* 2. Query SOundcloud API*/
-
+/* 2. Query Soundcloud API*/
+SC.initialize({
+    client_id: 'YOUR_CLIENT_ID'
+  });
+  
+  // find all sounds of buskers licensed under 'creative commons share alike'
+  SC.get('/tracks', {
+    q: 'buskers', license: 'cc-by-sa'
+  }).then(function(tracks) {
+    console.log(tracks);
+  });
 
 /* 3. Display the cards*/
 
