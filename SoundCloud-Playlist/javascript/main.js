74e9889fe2bea542a185fe6326cb1f1e347c89f2
Daniel Arvelo <darvelo00@gmail.com>
2020-01-09 19:08:02 -0500

    Created renderTracks function

--- SoundCloud-Playlist/javascript/main.js
+++ SoundCloud-Playlist/javascript/main.js
@@ -19,9 +19,52 @@ SoundCloudAPI.getTrack = function(inputValue) {
     });
 }
 
-/* 3. Display the cards*/
+SoundCloudAPI.renderTracks = function() {
+    //create 'card' div
+    var card = document.createElement('div');
+    card.classList.add("card");
+    document.querySelector('.js-search-results').appendChild(card);
 
+    //create 'image' div
+    var image_div = document.createElement('div');
+    image_div.classList.add("image");
+
+    var image_img = document.createElement('img');
+    image_img.classList.add("image_img");
+    image_img.src = "http://www.placekitten.com/290/290";
+    image_div.appendChild(image_img);
+
+    //create 'content' div
+    var content = document.createElement('div');
+    content.classList.add("content");
+
+    var header = document.createElement('div');
+    header.classList.add("header");
+    header.innerHTML = '<a href="#" target=\"_blank\">\"Science Vs. Romance\"</a>';
+    content.appendChild(header);
+
+    //create button
+    var button = document.createElement('div');
+    button.classList.add("ui", "bottom", "attached", "button", "js-button");
+
+    var icon = document.createElement('i');
+    icon.classList.add("add", "icon");
+    button.appendChild(icon);
+    
+    var buttonText = document.createElement('span');
+    buttonText.innerHTML = "Add to playlist";
+
+    //append to card
+    card.appendChild(image_div);
+    card.appendChild(content);
+    card.appendChild(button);
+}
+
+
+SoundCloudAPI.init();
+SoundCloudAPI.renderTracks();
+/* 3. Display the cards*/
+ 
 
 /* 4. Add to playlist and play*/
-SoundCloudAPI.init();
-SoundCloudAPI.getTrack("Rilo Kiley");
+
